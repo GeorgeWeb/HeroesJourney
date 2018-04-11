a0f0f74db1ccf6c3be408af894fff5cb32987603
Georgi Mirazchiyski <georgimweb@gmail.com>
2018-04-11 17:47:35 +0100

    Finishing mandatory - Fullscreen - WIP

--- engine/src/Engine/System/Settings.cpp
+++ engine/src/Engine/System/Settings.cpp
@@ -1,6 +1,5 @@
 #include "Settings.hpp"
 #include "../Engine.hpp"
-
 #include <iostream>
 
 #define NDEBUG 1
@@ -50,7 +49,35 @@ namespace Engine { namespace System {
 	{
 		// screen resolution
 		if (m_screenMode == SCREEN_MODE::WINDOWED)
-			Engine2D::GetWin().setSize(sf::Vector2u(m_screenWidth, m_screenHeight));
+		{
+			// reset game window
+			Engine2D::window->create(sf::VideoMode(1366, 768), Engine2D::title, 
+				sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
+			// Hide default cursor
+			Engine2D::window->setMouseCursorVisible(false);
+			// window size reset with the changed res
+			// Engine2D::GetWin().setSize(sf::Vector2u(m_screenWidth, m_screenHeight));
+			Renderer::Initialize(*Engine2D::window);
+			Engine2D::window->setSize(sf::Vector2u(m_screenWidth, m_screenHeight));
+			// init. game settigns
+			SetResolution(m_screenWidth, m_screenHeight);
+			// SetScreenMode(SCREEN_MODE::WINDOWED);
+			// SetVSyncOn(m_hasVsync);
+		}
+		else if (m_screenMode == SCREEN_MODE::FULLSCREEN)
+		{
+			// reset game window
+			Engine2D::window->create(sf::VideoMode(1366, 768), Engine2D::title, sf::Style::Fullscreen);
+			// Hide default cursor
+			Engine2D::window->setMouseCursorVisible(false);
+			// window size reset with the changed res
+			// init. renderer
+			Renderer::Initialize(*Engine2D::window);
+			// init. game settigns
+			SetResolution(m_screenWidth, m_screenHeight);
+			// SetScreenMode(SCREEN_MODE::WINDOWED);
+			// SetVSyncOn(m_hasVsync);
+		}
 
 		// v-sync on?
 		Engine2D::SetVsync(m_hasVsync);
